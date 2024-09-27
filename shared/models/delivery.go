e22c8f19d55d13e83db92cc417351058aeb556a3
Mohammad Hosein Aref <76614003+aref81@users.noreply.github.com>
2024-09-27 12:19:49 +0330

    Add Segment Logic

--- shared/models/delivery.go
+++ shared/models/delivery.go
@@ -5,61 +5,74 @@ import (
 	"github.com/aref81/snappbox_fare_estimator/shared/haversine"
 )
 
-// DeliveryPoint represents a single GPS coordination for a Delivery, each Delivery contains a sorted list of
-// these points
+// DeliveryPoint represents a single GPS coordination for a Delivery
 type DeliveryPoint struct {
 	Latitude  float64
 	Longitude float64
 	Timestamp int64
 }
 
-// Delivery represents an individual Delivery Data, which includes and ID and multiple Delivery Points
-type Delivery struct {
-	ID     int
-	Points []DeliveryPoint
+// DeliverySegment represents a segment of the road traveled, including two DeliveryPoints and Speed calculated for it.
+type DeliverySegment struct {
+	StartPoint DeliveryPoint
+	EndPoint   DeliveryPoint
+	Speed      float64
 }
 
-// AddPoint adds a new DeliveryPoint to the Delivery
-func (d *Delivery) AddPoint(lat, lng float64, timestamp int64) error {
-	newPoint := DeliveryPoint{
-		Latitude:  lat,
-		Longitude: lng,
-		Timestamp: timestamp,
+// Delivery represents an individual Delivery Data, which includes and ID and multiple DeliverySegments
+type Delivery struct {
+	ID       int
+	Segments []DeliverySegment
+}
+
+// AddSegment adds a new DeliverySegment to the Delivery after validation
+func (d *Delivery) AddSegment(startPoint DeliveryPoint, endPoint DeliveryPoint) error {
+	segmentSpeed, err := calculateSpeed(startPoint, endPoint)
+	if err != nil {
+		return err
 	}
 
-	if len(d.Points) != 0 {
-		err := validateDeliveryPoints(d.Points[len(d.Points)-1], newPoint)
-		if err != nil {
-			return err
-		}
+	segment := DeliverySegment{
+		StartPoint: startPoint,
+		EndPoint:   endPoint,
+		Speed:      segmentSpeed,
 	}
 
-	d.Points = append(d.Points, newPoint)
+	err = validateSegment(segment)
+	if err != nil {
+		return err
+	}
+
+	d.Segments = append(d.Segments, segment)
 	return nil
 }
 
 // NewDelivery initializes a new Delivery
 func NewDelivery(id int) *Delivery {
 	return &Delivery{
-		ID:     id,
-		Points: []DeliveryPoint{},
+		ID:       id,
+		Segments: []DeliverySegment{},
 	}
 }
 
-// ValidateDeliveryPoints checks the validity of two consecutive delivery points
-func validateDeliveryPoints(p1 DeliveryPoint, p2 DeliveryPoint) error {
+// ValidateDeliverySegment checks the validity of a segment by calculating the speed of it
+func validateSegment(segment DeliverySegment) error {
+	if segment.Speed <= 100.0 {
+		return nil
+	} else {
+		return fmt.Errorf("invalid delivery point, speed = %f", segment.Speed)
+	}
+}
+
+// calculateSpeed calculates the speed for a segment using haversine distance
+func calculateSpeed(p1 DeliveryPoint, p2 DeliveryPoint) (float64, error) {
 	timeDiff := float64(p2.Timestamp-p1.Timestamp) / 3600.0
 	if timeDiff == 0 {
 		// skipping zero time differences
-		return nil
+		return 0, fmt.Errorf("failed to calculate the speed, timeDiff = %f", timeDiff)
 	}
 
 	distance := haversine.Haversine(p1.Latitude, p1.Longitude, p2.Latitude, p2.Longitude)
 	speed := distance / timeDiff
-
-	if speed <= 100.0 {
-		return nil
-	} else {
-		return fmt.Errorf("invalid delivery point, speed = %f", speed)
-	}
+	return speed, nil
 }
